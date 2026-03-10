16dc8a122112c98f225cba8e1b13b60143aadd43
Mohaiminul Islam <MuhitiNix@users.noreply.github.com>
2026-03-10 19:56:41 +0600

    Connected core URLs to project root

--- blood_network/urls.py
+++ blood_network/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')), # This connects your core app to the main website
+    path('', include('core.urls')), # This tells Django to use the core URLs you just fixed
 ]
