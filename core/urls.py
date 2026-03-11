f3c5533d3f3d236c18e49da60bcec2e4e357eced
Mohaiminul Islam <MuhitiNix@users.noreply.github.com>
2026-03-12 00:31:40 +0600

    Bridge project URLs to core app

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Add this line below to make the home page work!
+    
     path('', views.search_donors, name='home'), 
     path('register/', views.register_page, name='register'),
     path('search/', views.search_donors, name='search'), 
